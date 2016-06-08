cc9a98980c2b550711f3da376a4848c2693927b4
Varun P M <varun.pm@ymedialabs.com>
2016-06-08 18:04:24 +0530

    Initial commit

---
+++ PageControl/PageControlViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PageControlViewController.swift
+//  PageControl
+//
+//  Created by Varun on 08/06/16.
+//  Copyright © 2016 VPM. All rights reserved.
+//
+
+import UIKit
+
+class PageControlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
