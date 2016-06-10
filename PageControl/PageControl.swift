8289118ac80e07794591871da8356874caa56329
Varun P M <varun.pm@ymedialabs.com>
2016-06-10 15:11:44 +0530

    Added delegate for handling the page changes

--- PageControl/PageControl.swift
+++ PageControl/PageControl.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+protocol PageControlDelegate : class {
+    // Called whenever the pageControl view is tapped.
+    func pageControl(pageControl : PageControl, didSelectPageIndex pageIndex : Int)
+}
+
 class PageControl: UIView {
     
     enum PageControlType : Int {
@@ -17,10 +22,12 @@ class PageControl: UIView {
         case SquareBorder // Square with border type. Border color will be representing the states
     }
     
-    private let pageControlSpacing : CGFloat = 10
-    private let pageControlWidth : CGFloat = 10
+    private let pageControlSpacing : CGFloat = 7
+    private let pageControlWidth : CGFloat = 7
     
-    var pageControlType = PageControlType.SquareBorder
+    var pageControlType = PageControlType.RoundedFilled
+    
+    weak var delegate : PageControlDelegate?
     
     @IBInspectable var numberOfPages : Int = 0 {
         didSet {
@@ -50,6 +57,34 @@ class PageControl: UIView {
         updatePageControlViews()
     }
     
+    //MARK: Handle the tap of page control
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touchObject = touches.first
+        
+        let startX = (bounds.size.width - CGFloat(numberOfPages) * pageControlWidth - CGFloat(numberOfPages - 1) * pageControlSpacing) / 2
+        let touchPoint = touchObject?.locationInView(self)
+        
+        if let touchPoint = touchPoint {
+            // pageControlWidth / 2 for midPoint identification
+            let tempPageControlSpacing = (touchPoint.x - startX - pageControlWidth / 2)
+            let selectedIndex = Int(round(tempPageControlSpacing / (pageControlWidth + pageControlSpacing)))
+            
+            if selectedIndex >= 0 && selectedIndex < numberOfPages {
+                // Reset the previous pageControlView color
+                let previousPageControlIndex = currentPage
+                currentPage = selectedIndex
+                
+                let pageControlView = viewWithTag(previousPageControlIndex + 1)
+                setUIForPageControlView(pageControlView, withIndex: previousPageControlIndex)
+                
+                let currentPageControlView = viewWithTag(currentPage + 1)
+                setUIForPageControlView(currentPageControlView, withIndex: currentPage)
+                
+                delegate?.pageControl(self, didSelectPageIndex: currentPage)
+            }
+        }
+    }
+    
     //MARK: Update UI
     private func updatePageControlViews() {
         // Calculate the start X point for first page control
@@ -58,34 +93,37 @@ class PageControl: UIView {
         for pageControlIndex in 0.stride(to: numberOfPages, by: 1) {
             let pageControlView = viewWithTag(pageControlIndex + 1)
             pageControlView?.center = CGPoint(x: (startX + pageControlWidth / 2), y: bounds.midY)
-            
-            switch pageControlType {
-            case .RoundedFilled:
-                pageControlView?.layer.cornerRadius = pageControlWidth / 2
-                pageControlView?.layer.masksToBounds = true
-                pageControlView?.layer.borderWidth = 0.0
-                pageControlView?.backgroundColor = getPageControlColorForIndex(pageControlIndex)
-                
-            case .RoundedBorder:
-                pageControlView?.layer.cornerRadius = pageControlWidth / 2
-                pageControlView?.layer.masksToBounds = true
-                pageControlView?.layer.borderWidth = 1.0
-                pageControlView?.layer.borderColor = getPageControlColorForIndex(pageControlIndex).CGColor
-                
-            case .SquareFilled:
-                pageControlView?.layer.cornerRadius = 0.0
-                pageControlView?.layer.masksToBounds = true
-                pageControlView?.layer.borderWidth = 0.0
-                pageControlView?.backgroundColor = getPageControlColorForIndex(pageControlIndex)
-                
-            case .SquareBorder:
-                pageControlView?.layer.cornerRadius = 0.0
-                pageControlView?.layer.masksToBounds = true
-                pageControlView?.layer.borderWidth = 1.0
-                pageControlView?.layer.borderColor = getPageControlColorForIndex(pageControlIndex).CGColor
-            }
+            setUIForPageControlView(pageControlView, withIndex: pageControlIndex)
             
             startX += pageControlSpacing + pageControlWidth
+        }
+    }
+    
+    private func setUIForPageControlView(pageControlView : UIView?, withIndex pageControlIndex : Int) {
+        switch pageControlType {
+        case .RoundedFilled:
+            pageControlView?.layer.cornerRadius = pageControlWidth / 2
+            pageControlView?.layer.masksToBounds = true
+            pageControlView?.layer.borderWidth = 0.0
+            pageControlView?.backgroundColor = getPageControlColorForIndex(pageControlIndex)
+            
+        case .RoundedBorder:
+            pageControlView?.layer.cornerRadius = pageControlWidth / 2
+            pageControlView?.layer.masksToBounds = true
+            pageControlView?.layer.borderWidth = 1.0
+            pageControlView?.layer.borderColor = getPageControlColorForIndex(pageControlIndex).CGColor
+            
+        case .SquareFilled:
+            pageControlView?.layer.cornerRadius = 0.0
+            pageControlView?.layer.masksToBounds = true
+            pageControlView?.layer.borderWidth = 0.0
+            pageControlView?.backgroundColor = getPageControlColorForIndex(pageControlIndex)
+            
+        case .SquareBorder:
+            pageControlView?.layer.cornerRadius = 0.0
+            pageControlView?.layer.masksToBounds = true
+            pageControlView?.layer.borderWidth = 1.0
+            pageControlView?.layer.borderColor = getPageControlColorForIndex(pageControlIndex).CGColor
         }
     }
     
@@ -98,7 +136,6 @@ class PageControl: UIView {
     
     private func getPageControlForIndex(index : Int) -> UIView {
         let pageControlView = UIView(frame: CGRect(x: 0, y: 0, width: pageControlWidth, height: pageControlWidth))
-        pageControlView.backgroundColor = getPageControlColorForIndex(index)
         pageControlView.tag = index + 1
         
         return pageControlView
--- PageControl/PageControlViewController.swift
+++ PageControl/PageControlViewController.swift
@@ -10,13 +10,42 @@ import UIKit
 
 class PageControlViewController: UIViewController {
 
+    @IBOutlet weak var pageControlView: PageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        pageControlView.numberOfPages = 4
+        pageControlView.currentPage = 0
+        pageControlView.pageIndicatorTintColor = UIColor.blackColor()
+        pageControlView.currentPageIndicatorTintColor = UIColor.redColor()
+        pageControlView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+extension PageControlViewController : PageControlDelegate {
+    func pageControl(pageControl: PageControl, didSelectPageIndex pageIndex: Int) {
+        switch pageIndex {
+        case 0:
+            pageControl.backgroundColor = UIColor.whiteColor()
+            
+        case 1:
+            pageControl.backgroundColor = UIColor.yellowColor()
+            
+        case 2:
+            pageControl.backgroundColor = UIColor.greenColor()
+            
+        case 3:
+            pageControl.backgroundColor = UIColor.blueColor()
+            
+        default:
+            pageControl.backgroundColor = UIColor.whiteColor()
+        }
     }
 }
